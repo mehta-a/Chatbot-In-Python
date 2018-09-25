fb21906d8461577357511bd9546218104f2d9d42
amehta02 <ankita.mehta@factset.com>
2018-09-25 18:31:58 +0530

    EchoBot I Ready

--- src/echobot.py
+++ src/echobot.py
@@ -9,5 +9,16 @@ def respond(message):
     # Return the result
     return bot_message
 
+
+# Define a function that sends a message to the bot: send_message
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+
 if __name__ == "__main__":
-    print(respond("Hi how are you!"))
+   send_message("Hi! How are you?")
