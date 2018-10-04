358928d5f90438fef7a9f72806d82161ca76a6ea
amehta02 <ankita.mehta@factset.com>
2018-10-05 00:59:38 +0530

    Greet Bot is here! It has just got smarter.

--- src/2. Understanding Natural Language Queries/GreetIntent.py
+++ src/2. Understanding Natural Language Queries/GreetIntent.py
@@ -9,17 +9,7 @@ responses = {'greet': 'Hello you! :)',
             'goodbye': 'goodbye for now'}
 
 
-# Define a function that sends a message to the bot: send_message
-def send_message(message):
-    # Print user_template including the user_message
-    print(user_template.format(message))
-    # Get the bot's response to the message
-    response = respond(message)
-    # Print the bot template including the bot's response.
-    print(bot_template.format(response))
-
-
-# keywords to remmmebr patterns
+# keywords to remember patterns
 keywords = {'greet': ['hello', 'hi', 'hey'],
             'thankyou': ['thank', 'thx'],
             'goodbye': ['bye', 'farewell']}
@@ -33,6 +23,7 @@ for intent, keys in keywords.items():
     # Create regular expressions and compile them into pattern objects
     patterns[intent] = re.compile('|'.join(keys))
 
+
 # Define a function to find the intent of a message
 def match_intent(message):
     matched_intent = None
@@ -41,6 +32,7 @@ def match_intent(message):
         if pattern.search(message):
             matched_intent = intent
     return matched_intent
+
 
 # Define a respond function
 def respond(message):
@@ -51,6 +43,17 @@ def respond(message):
     if intent in responses:
         key = intent
     return responses[key]
+
+
+# Define a function that sends a message to the bot: send_message
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
 
 # Send messages
 send_message("hello!")
